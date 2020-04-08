bc6dcdf7b6e920888353dfe874140bff10662925
Kauan C. S <kauan13cs@gmail.com>
2020-04-08 17:07:12 -0300

    Updates

---
+++ precache-manifest.881e1a503bde57358ac703fc4db585a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af1649cd42bda326ab2729f220ad05d",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "ad984c3c84caa0e36672",
+    "url": "/portifolio/static/css/main.ed4680ff.chunk.css"
+  },
+  {
+    "revision": "acd0ce544df9f28f4767",
+    "url": "/portifolio/static/js/2.ef231a91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portifolio/static/js/2.ef231a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad984c3c84caa0e36672",
+    "url": "/portifolio/static/js/main.c4667806.chunk.js"
+  },
+  {
+    "revision": "125541ecb42d1c013fe7",
+    "url": "/portifolio/static/js/runtime-main.3976501c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
