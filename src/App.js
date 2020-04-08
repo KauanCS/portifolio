b0dd7b0f2fb23a9bab6be163fbf577c6594c3bd8
Kauan C. S <kauan13cs@gmail.com>
2020-04-08 09:20:40 -0300

    Boas vindas - update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BEM-VINDO AI SEU TROXA
+          BEM-VINDO AI SEU TROXA KKKKKKKK
         </p>
         <a
           className="App-link"
